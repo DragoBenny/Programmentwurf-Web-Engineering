c10533985c60d2c98b9608c74c4325adf3b7a341
DragoBenny <superminka887@gmail.com>
2026-01-27 11:26:31 +0100

    initialized mariadb

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
+const controller = require("../controller/user.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("C O C K   I N J U R E D");
+  if(res.send("bup")){
+    return 0;
+  }
+  res.status(500);
 });
 
 module.exports = router;
